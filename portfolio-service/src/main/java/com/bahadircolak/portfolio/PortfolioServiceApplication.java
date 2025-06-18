95d125620ec039f71fdab3a0885c28f18c6c71df
Bahadır Çolak <bahadircolak34@gmail.com>
2025-06-18 23:01:58 +0300

    services fixed

--- portfolio-service/src/main/java/com/bahadircolak/portfolio/PortfolioServiceApplication.java
+++ portfolio-service/src/main/java/com/bahadircolak/portfolio/PortfolioServiceApplication.java
@@ -2,8 +2,10 @@ package com.bahadircolak.portfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.bahadircolak.portfolio", "com.bahadircolak.common"})
 public class PortfolioServiceApplication {
 
     public static void main(String[] args) {
--- wallet-service/src/main/java/com/bahadircolak/wallet/WalletServiceApplication.java
+++ wallet-service/src/main/java/com/bahadircolak/wallet/WalletServiceApplication.java
@@ -3,9 +3,11 @@ package com.bahadircolak.wallet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@ComponentScan(basePackages = {"com.bahadircolak.wallet", "com.bahadircolak.common"})
 public class WalletServiceApplication {
 
     public static void main(String[] args) {
